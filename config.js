6c63022244075e4ebe64512b5f20cd5299632b3b
Kamidake <x9k92007@gmail.com>
2025-07-16 00:48:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const baseConfig = {
     port: process.env.PORT || 10000,
-    defaultUserAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+    defaultUserAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 VLC/3.0.20 LibVLC/3.0.20',
     cacheSettings: {
         updateInterval: 2 * 60 * 60 * 1000,
         maxAge: 12 * 60 * 60 * 1000,
