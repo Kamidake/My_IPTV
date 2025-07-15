76cfc2ac18cdf112a3aded72f8121558503c44f9
Kamidake <x9k92007@gmail.com>
2025-07-15 12:43:27 +0800

    Update meta-handler.js

--- meta-handler.js
+++ meta-handler.js
@@ -85,7 +85,7 @@ async function metaHandler({ type, id, config: userConfig }) {
 
 
         const meta = {
-            id: '',
+            id: channel.name,
             type: 'tv',
             name: channel.streamInfo?.tvg?.chno 
                 ? `${channel.streamInfo.tvg.chno}. ${channel.name}`
@@ -102,7 +102,7 @@ async function metaHandler({ type, id, config: userConfig }) {
             isFree: true,
             behaviorHints: {
                 isLive: true,
-                defaultVideoId: ''
+                defaultVideoId: channel.name
             }
         };
 
